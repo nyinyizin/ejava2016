f48941660a8880b363e6910e92e8b97aa6351d58
XingYong <kekxingyong@gmail.com>
2016-10-22 15:48:30 +0800

    Added "Body" into Appointment & People

--- ca1/src/java/ca1/model/Appointment.java
+++ ca1/src/java/ca1/model/Appointment.java
@@ -5,7 +5,90 @@
  */
 package ca1.model;
 
+import java.io.Serializable;
+import java.sql.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
-public class Appointment {
+@Entity
+@Table(name="appointment")
+public class Appointment implements Serializable{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "appt_id")
+	private Integer appointmentId;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "appt_date")
+    private Date apptDate;
+    
+    @ManyToOne
+    @JoinColumn( name = "pid", referencedColumnName = "pid")
+    private People people;
+
+    /**
+     * @return the appointmentId
+     */
+    public Integer getAppointmentId() {
+        return appointmentId;
+    }
+
+    /**
+     * @param appointmentId the appointmentId to set
+     */
+    public void setAppointmentId(Integer appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the apptDate
+     */
+    public Date getApptDate() {
+        return apptDate;
+    }
+
+    /**
+     * @param apptDate the apptDate to set
+     */
+    public void setApptDate(Date apptDate) {
+        this.apptDate = apptDate;
+    }
+
+    /**
+     * @return the people
+     */
+    public People getPeople() {
+        return people;
+    }
+
+    /**
+     * @param people the people to set
+     */
+    public void setPeople(People people) {
+        this.people = people;
+    }
+    
     
 }
